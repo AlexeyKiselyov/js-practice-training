3cb15cfe9fbb3779688ec44bfb974576f2a714f5
AlexeyKiselyov <aleshakiselev123@gmail.com>
2023-02-07 14:20:16 +0200

    resolve convertToRoman with while

--- TaskExersises/TaskExersises.js
+++ TaskExersises/TaskExersises.js
@@ -888,6 +888,21 @@
 // 30----------Convert To Roman------------
 
 // first option
+function convertToRoman(num) {
+  var ref = [['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90], ['L', 50], ['XL', 40], ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]];
+  var res = [];
+  ref.forEach(function(p) {
+    while (num >= p[1]) {
+      res.push(p[0]);
+      num -= p[1];
+    }
+  });
+  return res.join('');
+}
+
+console.log(convertToRoman(1000));
+
+// second option
 // const convertToRoman = (numb) => {
 //   const arr = numb.toString().split("");
 //   let result = [];
@@ -1020,7 +1035,7 @@
 
 // console.log(convertToRoman(3999));
 
-// second option not optimaze
+// third option not optimaze
 // const convertToRoman = (numb) => {
 //   const arr = numb.toString().split("");
 //   let result = [];
