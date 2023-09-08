9c6f77527754095051a6a064ff9e1d9ecfcbbebc
AlexeyKiselyov <aleshakiselev123@gmail.com>
2023-09-08 22:35:56 +0300

    js training

--- CodeWars/codeWars.js
+++ CodeWars/codeWars.js
@@ -1607,6 +1607,14 @@
 //   return result.join('.');
 // }
 
-console.log(wordPattern('helLo')); // "0.1.2.2.3"
+// console.log(wordPattern('helLo')); // "0.1.2.2.3"
 
 // console.log(wordPattern('Hippopotomonstrosesquippedaliophobia')); // "0.1.2.2.3.2.3.4.3.5.3.6.7.4.8.3.7.9.7.10.11.1.2.2.9.12.13.14.1.3.2.0.3.15.1.13"
+
+// ----------All Star Code Challenge #3--------------
+
+// var removeVowels = function (str) {
+//   return str.replace(/[aeiouAEIOU]/g, '');
+// };
+
+// console.log(removeVowels('drake')); //"drk"
