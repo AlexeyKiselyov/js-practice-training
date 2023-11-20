91a41a7773681c09468f1af818c43b558b306e1d
AlexeyKiselyov <aleshakiselev123@gmail.com>
2023-11-20 19:53:42 +0200

    ts training

--- TS/codewars.js
+++ TS/codewars.js
@@ -1,12 +1,12 @@
+"use strict";
 // ---------Vowel Count-------------
-
 function getCount(str) {
-  var vowelsList = ['a', 'e', 'i', 'o', 'u'];
-  return str.split('').reduce(function (acc, char) {
-    if (vowelsList.indexOf(char) >= 0) {
-      acc += 1;
-    }
-    return acc;
-  }, 0);
+    const vowelsList = ['a', 'e', 'i', 'o', 'u'];
+    return str.split('').reduce((acc, char) => {
+        if (vowelsList.includes(char)) {
+            acc += 1;
+        }
+        return acc;
+    }, 0);
 }
 console.log(getCount('abracadabra'));
--- TS/codewars.ts
+++ TS/codewars.ts
@@ -3,8 +3,8 @@
 // function getCount(str: string): number {
 //   const vowelsList = ['a', 'e', 'i', 'o', 'u'];
 
-//   return str.split('').reduce((acc: number, char: string): number => {
-//     if (vowelsList.indexOf(char) >= 0) {
+//   return str.split('').reduce((acc, char: string) => {
+//     if (vowelsList.includes(char)) {
 //       acc += 1;
 //     }
 //     return acc;
@@ -12,3 +12,50 @@
 // }
 
 // console.log(getCount('abracadabra'));
+
+// -----------Split Strings----------
+
+// 1st option
+// function solution(str: string): string[] {
+//   const result: string[] = [];
+
+//   str.split('').forEach((char, ind, arr) => {
+//     if (ind % 2 === 0) {
+//       result.push(char + (arr[ind + 1] || '_'));
+//     }
+//   });
+
+//   return result;
+// }
+
+// 2nd option
+// function solution(str: string): string[] {
+//   const splitedStr = str.split('');
+
+//   return splitedStr
+//     .map((char, ind) => char + (splitedStr.splice(ind + 1, 1)[0] || '_'))
+//     .filter(el => el);
+// }
+
+// 3rd option
+// function solution(str: string): string[] {
+//   let tempStr: string = '';
+//   const result: string[] = [];
+
+//   str.split('').forEach((char, ind, arr) => {
+//     if (ind % 2 !== 0) {
+//       tempStr += char;
+//       result.push(tempStr);
+//       tempStr = '';
+//     } else if (ind === arr.length - 1 && ind % 2 === 0) {
+//       result.push(char + '_');
+//     } else {
+//       tempStr += char;
+//     }
+//   });
+
+//   return result;
+// }
+
+// console.log(solution('abc')); // ['ab', 'c_']
+// console.log(solution('abcdef')); // ['ab', 'cd', 'ef']
