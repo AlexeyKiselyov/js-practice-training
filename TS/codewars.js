b9d16dd46e37bb5d4e613846d938a76e517981d9
AlexeyKiselyov <aleshakiselev123@gmail.com>
2023-11-21 20:11:33 +0200

    ts training

--- TS/codewars.js
+++ TS/codewars.js
@@ -1,12 +1,60 @@
-"use strict";
+'use strict';
 // ---------Vowel Count-------------
-function getCount(str) {
-    const vowelsList = ['a', 'e', 'i', 'o', 'u'];
-    return str.split('').reduce((acc, char) => {
-        if (vowelsList.includes(char)) {
-            acc += 1;
-        }
-        return acc;
-    }, 0);
+function zero(operation) {
+  return operation ? operation(0) : 0;
 }
-console.log(getCount('abracadabra'));
+function one(operation) {
+  return operation ? operation(1) : 1;
+}
+function two(operation) {
+  return operation ? operation(2) : 2;
+}
+function three(operation) {
+  return operation ? operation(3) : 3;
+}
+function four(operation) {
+  return operation ? operation(4) : 4;
+}
+function five(operation) {
+  return operation ? operation(5) : 5;
+}
+function six(operation) {
+  return operation ? operation(6) : 6;
+}
+function seven(operation) {
+  return operation ? operation(7) : 7;
+}
+function eight(operation) {
+  return operation ? operation(8) : 8;
+}
+function nine(operation) {
+  return operation ? operation(9) : 9;
+}
+
+function plus(rightOperand) {
+  return function (leftOperand) {
+    return leftOperand + rightOperand;
+  };
+}
+function minus(rightOperand) {
+  return function (leftOperand) {
+    return leftOperand - rightOperand;
+  };
+}
+function times(rightOperand) {
+  return function (leftOperand) {
+    return Math.floor(leftOperand * rightOperand);
+  };
+}
+function dividedBy(rightOperand) {
+  return function (leftOperand) {
+    if (rightOperand === 0) {
+      throw new Error('Cannot divide by zero');
+    }
+    return Math.floor(leftOperand / rightOperand);
+  };
+}
+console.log(seven(times(five()))); // must return 35
+console.log(four(plus(nine()))); // must return 13
+console.log(eight(minus(three()))); // must return 5
+console.log(six(dividedBy(two()))); // must return 3
--- TS/codewars.ts
+++ TS/codewars.ts
@@ -59,3 +59,70 @@
 
 // console.log(solution('abc')); // ['ab', 'c_']
 // console.log(solution('abcdef')); // ['ab', 'cd', 'ef']
+
+//-----Calculating with Functions----
+
+// type Operation = (operand: number) => number;
+
+// function zero(operation?: Operation): number {
+//   return operation ? operation(0) : 0;
+// }
+// function one(operation?: Operation): number {
+//   return operation ? operation(1) : 1;
+// }
+// function two(operation?: Operation): number {
+//   return operation ? operation(2) : 2;
+// }
+// function three(operation?: Operation): number {
+//   return operation ? operation(3) : 3;
+// }
+// function four(operation?: Operation): number {
+//   return operation ? operation(4) : 4;
+// }
+// function five(operation?: Operation): number {
+//   return operation ? operation(5) : 5;
+// }
+// function six(operation?: Operation): number {
+//   return operation ? operation(6) : 6;
+// }
+// function seven(operation?: Operation): number {
+//   return operation ? operation(7) : 7;
+// }
+// function eight(operation?: Operation): number {
+//   return operation ? operation(8) : 8;
+// }
+// function nine(operation?: Operation): number {
+//   return operation ? operation(9) : 9;
+// }
+
+// function plus(rightOperand: number): Operation {
+//   return function (leftOperand: number): number {
+//     return leftOperand + rightOperand;
+//   };
+// }
+
+// function minus(rightOperand: number): Operation {
+//   return function (leftOperand: number): number {
+//     return leftOperand - rightOperand;
+//   };
+// }
+
+// function times(rightOperand: number): Operation {
+//   return function (leftOperand: number): number {
+//     return Math.floor(leftOperand * rightOperand);
+//   };
+// }
+
+// function dividedBy(rightOperand: number): Operation {
+//   return function (leftOperand: number): number {
+//     if (rightOperand === 0) {
+//       throw new Error('Cannot divide by zero');
+//     }
+//     return Math.floor(leftOperand / rightOperand);
+//   };
+// }
+
+// console.log(seven(times(five()))); // must return 35
+// console.log(four(plus(nine()))); // must return 13
+// console.log(eight(minus(three()))); // must return 5
+// console.log(six(dividedBy(two()))); // must return 3
